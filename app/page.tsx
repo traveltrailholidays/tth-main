585707553d3d4ce62ada6f8b934399c50f98164d
Diwakar Jha <diwakarjha554@gmail.com>
2024-07-14 09:20:08 +0530

    error resolved

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import HomeHeroSection from '@/components/home/hero-section';
 import HomeSelectCategory from '@/components/home/select-category';
 import getCurrentUser from '@/frontend/actions/getCurrentUser';
 import React from 'react';
+import { Suspense } from 'react';
 
 const page = async () => {
 
@@ -15,9 +16,12 @@ const page = async () => {
       <Header currentUser={currentUser}/>
       <HomeHeroSection />
       <ExplorePackages />
-      <HomeSelectCategory />
+      <Suspense> {/* I have to use this, because i used the useSearchParams in select category component */}
+        <HomeSelectCategory />
+      </Suspense>
     </ClientOnly>
   )
 }
 
 export default page;
+
